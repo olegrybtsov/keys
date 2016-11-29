b5f221f1665651fd775f2b4b096724b505011495
olegrybtsov <oleg.rybtsov@gmail.com>
2016-11-29 22:32:21 +0200

    Create keys.tf

---
+++ keys.tf
@@ -0,0 +1,2 @@
+variable "aws_access_key" { description = "Amazon access key id" default = "YOU_AMAZON_ACCESS_KEY" }
+variable "aws_secret_key" { description = "Amazon secret access key" default = "YOU_AMAZON_SECRET_KEY" }
