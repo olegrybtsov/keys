ada249e661b3366e2d351716958e7e61bae81f0b
olegrybtsov <oleg.rybtsov@gmail.com>
2016-11-30 00:57:37 +0200

    Delete keys.tf

--- keys.tf
+++
@@ -1,2 +0,0 @@
-variable "aws_access_key" { description = "Amazon access key id" default = "AKIAJNXZFHP7S7IWU2AA" }
-variable "aws_secret_key" { description = "Amazon secret access key" default = "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" }
